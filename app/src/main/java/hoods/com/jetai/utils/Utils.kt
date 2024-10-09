0e433cf873a8626ad038d22cc9f81370cb8b9876
Sandra <kuriasandra651@gmail.com>
2024-10-09 23:50:35 +0300

    chatroom screen

--- app/src/main/java/hoods/com/jetai/utils/Utils.kt
+++ app/src/main/java/hoods/com/jetai/utils/Utils.kt
@@ -1,10 +1,19 @@
 package hoods.com.jetai.utils
 
 import android.util.Patterns
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
 import java.util.regex.Pattern
 
 fun isValidEmail (email:String):Boolean{
     if (email.isEmpty()) return false
     val emailPattern = Patterns.EMAIL_ADDRESS
     return emailPattern.matcher(email).matches()
+}
+
+fun formatDate(date: Date):String {
+    val pattern = "yyyy-MM-dd  HH:mm:ss"
+    val sdf = SimpleDateFormat(pattern, Locale.getDefault())
+    return sdf.format(date)
 }
