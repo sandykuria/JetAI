6ae038c98d7bc3de537b02313b2dff820431db5a
Sandra <kuriasandra651@gmail.com>
2024-10-10 00:34:13 +0300

    Bottom and top app bar logic

--- app/src/main/java/hoods/com/jetai/MainActivity.kt
+++ app/src/main/java/hoods/com/jetai/MainActivity.kt
@@ -3,6 +3,7 @@ package hoods.com.jetai
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
@@ -10,19 +11,25 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import hoods.com.jetai.authentication.register.SignUpScreen
 import hoods.com.jetai.authentication.register.login.LoginScreen
 import hoods.com.jetai.authentication.register.login.LoginViewModel
+import hoods.com.jetai.chatroom.components.BottomNavBar
 import hoods.com.jetai.navigation.JetAiNavGraph
 import hoods.com.jetai.navigation.JetAiNavigationActions
 import hoods.com.jetai.navigation.Route
+import hoods.com.jetai.navigation.Tabs
 import hoods.com.jetai.ui.theme.JetAiTheme
 
 class MainActivity : ComponentActivity() {
@@ -51,8 +58,49 @@ class MainActivity : ComponentActivity() {
         }
         val loginViewModel:LoginViewModel = viewModel()
         val authState by remember { mutableStateOf(loginViewModel.hasUserVerified()) }
+        val startRoute = if (authState) Route.NESTED_HOME_ROUTE else Route.NESTED_AUTH_ROUTE
+        val stateDestination by navController.currentBackStackEntryAsState()
+        val currentDestination = stateDestination?.destination?.route
+        val isChatRoomDestination = currentDestination == Tabs.Chats.title
+        val isHomeDestination =
+            currentDestination == Route.NESTED_HOME_ROUTE
+                    || currentDestination == Tabs.Chats.title
+                    ||currentDestination == Tabs.VisualIq.title
+        var selectedTabIndex by remember { mutableIntStateOf(0) }
+        val tabs = Tabs.entries.toList()
 
-        Scaffold {innerPadding ->
+        LaunchedEffect(stateDestination) {
+            when (currentDestination){
+                Tabs.Chats.title -> {
+                    selectedTabIndex = 0
+                }
+                Tabs.VisualIq.title -> {
+                    selectedTabIndex = 1
+                }
+            }
+        }
+
+        Scaffold(
+            bottomBar = {
+                AnimatedVisibility(isHomeDestination) {
+                    BottomNavBar(
+                        tabs = tabs,
+                        selectedIndex = selectedTabIndex,
+                        onSelectedChange = {
+                            selectedTabIndex = it
+                            when(it){
+                                0 -> {
+                                    navAction.navigateToHomeGraph()
+                                }
+                                1 -> {
+                                    navAction.navigateToVisualIqScreen()
+                                }
+                            }
+                        }
+                    )
+                }
+            }
+        ) {innerPadding ->
             JetAiNavGraph(
                 navController = navController,
                 navAction = navAction,
